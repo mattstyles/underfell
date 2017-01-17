7a5640225c3493cb563aff05a71905e817efb1ee
Matt Styles <matt@personalurban.com>
2017-01-17 06:46:01 +0000

    update break on blocking cells

--- src/core/updates/visibility.js
+++ src/core/updates/visibility.js
@@ -104,41 +104,41 @@ const get = (mat, x, y) => {
  * It'll bail if it hits a solid block, firing the callback on the way out.
  * @deprecated in favour of using Ray.cast and generators
  */
-const castRay = (mat, ray, light, cb) => {
-  // Check for 0 length ray which would create an infinite while
-  if (ray.length() <= 0.1) {
-    return
-  }
-
-  let r = ray
-  let [x, y] = light.position
-
-  // Cast the ray by incrementing its magnitude
-  while (r.length() < light.magnitude) {
-    // Get ray current position
-    let [i, j] = r.position()
-
-    // Offset and integerise
-    let [u, v] = [
-      x + VISIBILITY.ORIGIN_OFFSET + i | 0,
-      y + VISIBILITY.ORIGIN_OFFSET + j | 0
-    ]
-
-    let cell = get(mat, u, v)
-    if (cell) {
-      // Pass back the cell, the extent of the raycast and the position
-      cb(cell, 1 - (r.length() / light.magnitude), u, v)
-    }
-
-    // Perform check to see if this ray should continue casting
-    if (isBlocker(mat, u, v)) {
-      break
-    }
-
-    // Grow ray scalar to increment cast
-    r = r.scalar(VISIBILITY.RAY_MAG_INC)
-  }
-}
+// const castRay = (mat, ray, light, cb) => {
+//   // Check for 0 length ray which would create an infinite while
+//   if (ray.length() <= 0.1) {
+//     return
+//   }
+//
+//   let r = ray
+//   let [x, y] = light.position
+//
+//   // Cast the ray by incrementing its magnitude
+//   while (r.length() < light.magnitude) {
+//     // Get ray current position
+//     let [i, j] = r.position()
+//
+//     // Offset and integerise
+//     let [u, v] = [
+//       x + VISIBILITY.ORIGIN_OFFSET + i | 0,
+//       y + VISIBILITY.ORIGIN_OFFSET + j | 0
+//     ]
+//
+//     let cell = get(mat, u, v)
+//     if (cell) {
+//       // Pass back the cell, the extent of the raycast and the position
+//       cb(cell, 1 - (r.length() / light.magnitude), u, v)
+//     }
+//
+//     // Perform check to see if this ray should continue casting
+//     if (isBlocker(mat, u, v)) {
+//       break
+//     }
+//
+//     // Grow ray scalar to increment cast
+//     r = r.scalar(VISIBILITY.RAY_MAG_INC)
+//   }
+// }
 
 /**
  * Flags a chunk as dirty, used when a cell changes to flag to the renderer to
@@ -148,7 +148,9 @@ const castRay = (mat, ray, light, cb) => {
  * slower when querying chunks at the bottom of the list and sometimes gets
  * super slow. maybe if the chunk array was divided into x, y as those values
  * can be calculated from cell [x, y] and chunk dimensions rather than a linear
- * find operation being spammed.
+ * find operation being spammed. a for loop instead of a find might be enough
+ * to make this function tolerable speedwise, its necessary to consider as its
+ * the only way to get React cranked up to displaying a large number of tiles.
  */
 const updateChunk = (chunks, x, y) => {
   return
@@ -182,19 +184,36 @@ export const updateVisibility = (map, char) => {
       ],
       magnitude: char.magnitude,
       step: VISIBILITY.RAY_MAG_INC
-    })
+    })()
 
     // Turn into a while loop and terminate either at a blocker
     // or when the ray reaches its angle, i.e. next === false
-    for (let p of cast()) {
-      let cell = get(mat, p[0] | 0, p[1] | 0)
-      if (cell) {
-        // Dummy all light cells to 1, @TODO update lighting calcs
-        // cell.light = 1
+    // for (let p of cast()) {
+    //   let cell = get(mat, p[0] | 0, p[1] | 0)
+    //   if (cell) {
+    //     // Dummy all light cells to 1, @TODO update lighting calcs
+    //     // cell.light = 1
+    //
+    //     makeCellVisible(cell)
+    //     updateChunk(map.chunks, p[0] | 0, p[1] | 0)
+    //   }
+    // }
 
+    let p = cast.next()
+    while (!p.done) {
+      let [u, v] = [p.value[0] | 0, p.value[1] | 0]
+      let cell = get(mat, u, v)
+
+      if (cell) {
         makeCellVisible(cell)
-        updateChunk(map.chunks, p[0] | 0, p[1] | 0)
+        updateChunk(map.chunks, u, v)
       }
+
+      if (isBlocker(mat, u, v)) {
+        break
+      }
+
+      p = cast.next()
     }
   }
 
@@ -230,14 +249,16 @@ const updateLightmap = (map, light) => {
       light.position[1] + VISIBILITY.ORIGIN_OFFSET
     ]
     let d = 1 - (distance(point, pos) / light.magnitude)
+    // The easing function can get a little funky with non 0...1 values
+    if (d < 0) {
+      return 0.25
+    }
     return clamp(easing(d), 0.25, 1)
   }
 
   // Adds a light level to the cell referred to by point
-  function updateCellLight (point) {
-    let x = point[0] | 0
-    let y = point[1] | 0
-    let cell = get(mat, x, y)
+  function updateCellLight (u, v) {
+    let cell = get(mat, u, v)
 
     if (!cell) {
       return false
@@ -247,17 +268,16 @@ const updateLightmap = (map, light) => {
     // Bail if this light has already visited this cell
     for (let i = 0; i < closed.length; i++) {
       let pt = closed[i]
-      if (pt[0] === x && pt[1] === y) {
+      if (pt[0] === u && pt[1] === v) {
         return false
       }
     }
-    closed.push([x, y])
+    closed.push([u, v])
 
-    let l = getLuminance(point)
+    let l = getLuminance([u, v])
     cell.light += l
 
-    //  makeCellVisible(cell)
-    //  updateChunk(map.chunks, p[0] | 0, p[1] | 0)
+    updateChunk(map.chunks, u, v)
     return true
   }
 
@@ -275,85 +295,36 @@ const updateLightmap = (map, light) => {
       ],
       magnitude: light.magnitude,
       step: VISIBILITY.RAY_MAG_INC
-    })
+    })()
 
     // Turn into a while loop and terminate either at a blocker
     // or when the ray reaches its angle, i.e. next === false
-    for (let p of cast()) {
-      updateCellLight(p)
+    // for (let p of cast()) {
+    //   updateCellLight(p)
+    // }
+
+    let p = cast.next()
+    while (!p.done) {
+      let [u, v] = [p.value[0] | 0, p.value[1] | 0]
+
+      updateCellLight(u, v)
+
+      if (isBlocker(mat, u, v)) {
+        break
+      }
+
+      p = cast.next()
     }
   }
 
   // Update light source block to be fully lighted
   let source = get(mat, ...light.position)
   if (source) {
-    console.log(source)
     source.light = 1
   }
 
   return map
 }
-
-// const updateLightmap = (map, light) => {
-//   let mat = ndarray(map.data, [map.width, map.height])
-//   let ray = new Vector2(1, 0)
-//
-//   // Keep track of cells we have already visited and only add light once
-//   let closed = []
-//
-//   // Updates the cell with light calculation
-//   const updateCellLight = (cell, lumination, x, y) => {
-//     // If we have already visited this cell then we're good to skip it,
-//     // another light source could visit and up the light level though.
-//     if (closed.find(cell => cell[0] === x && cell[1] === y)) {
-//       return
-//     }
-//
-//     closed.push([x, y])
-//
-//     // Clamp luminosity 0.25...1.
-//     // Apply easing to the luminosity to create a better light falloff.
-//     // @TODO light describes block opacity currently but better could be
-//     // to have coloured lights so cell.light would become a magnitude and
-//     // a colour, the renderer would then perform the blend.
-//     // @TODO this 0.25 could be extracted to a global, or ambient, light
-//     // level for all blocks.
-//     let l = 0.25 + (easing(lumination) * 0.75)
-//
-//     // Use light addition so that multiple light sources stack
-//     cell.light += l
-//
-//     // Clamp saturation to 1
-//     if (cell.light > 1) {
-//       cell.light = 1
-//     }
-//
-//     // Update the chunk dirty flag
-//     // No longer using chunks, this function caused huge lag spikes
-//     // updateChunk(map.chunks, x, y)
-//   }
-//
-//   for (
-//     let angle = light.startAngle;
-//     angle < light.endAngle;
-//     angle += VISIBILITY.RAY_ANGLE_INC
-//   ) {
-//     castRay(
-//       mat,
-//       ray.rotate(angle),
-//       light,
-//       updateCellLight
-//     )
-//   }
-//
-//   // Update light source block to be fully lighted
-//   let source = get(mat, ...light.position)
-//   if (source) {
-//     source.light = 1
-//   }
-//
-//   return map
-// }
 
 /**
  * Work the matrix through all light sources, applying light transforms as
@@ -373,86 +344,4 @@ export const updateLights = (map, lights, vision) => {
       return distance(light.position, vision.position) < vision.magnitude + light.magnitude
     })
     .reduce(updateLightmap, map)
-}
-
-export const rayman = (map, pos) => {
-  let mat = ndarray(map.data, [map.width, map.height])
-
-  // Set standing tile visible
-  let cell = get(mat, ...pos)
-  if (cell) {
-    cell.light = 1
-    makeCellVisible(cell)
-    updateChunk(map.chunks, ...pos)
-  }
-
-  for (let a = 0; a < Math.PI * 2; a += VISIBILITY.RAY_ANGLE_INC) {
-    let d = Vector2.fromAngle(a)
-    let ray = new Ray(d)
-    let cast = ray.project({
-      origin: [pos[0] + 0.5, pos[1] + 0.5],
-      magnitude: 6,
-      step: 1
-    })
-
-    cast(vec => {
-      let p = vec.position()
-      let cell = get(mat, p[0] | 0, p[1] | 0)
-      if (cell) {
-        cell.light = 1
-        makeCellVisible(cell)
-        // updateChunk(map.chunks, p[0] | 0, p[1] | 0)
-      }
-    })
-
-    // let cast = ray.cast({
-    //   origin: [pos[0] + 0.5, pos[1] + 0.5],
-    //   magnitude: 6,
-    //   step: 1
-    // })
-
-    // Using the generator
-    // for (let p of cast()) {
-    //   let cell = get(mat, p[0] | 0, p[1] | 0)
-    //   if (cell) {
-    //     cell.light = 1
-    //     makeCellVisible(cell)
-    //     // updateChunk(map.chunks, p[0] | 0, p[1] | 0)
-    //   }
-    // }
-  }
-
-  // let dir = Vector2.fromAngle(0)
-  // let ray = new Ray(dir)
-  // let cast = ray.cast({
-  //   origin: [pos[0] + 0.5, pos[1] + 0.5],
-  //   magnitude: 6,
-  //   step: 1
-  // })
-  //
-  // for (let p of cast()) {
-  //   let cell = get(mat, p[0] | 0, p[1] | 0)
-  //   if (cell) {
-  //     cell.light = 1
-  //     makeCellVisible(cell)
-  //     updateChunk(map.chunks, p[0] | 0, p[1] | 0)
-  //   }
-  // }
-  //
-  // ray = new Ray(Vector2.fromAngle(Math.PI))
-  // cast = ray.cast({
-  //   origin: [pos[0] + 0.5, pos[1] + 0.5],
-  //   magnitude: 6,
-  //   step: 1
-  // })
-  // for (let p of cast()) {
-  //   let cell = get(mat, p[0] | 0, p[1] | 0)
-  //   if (cell) {
-  //     cell.light = 1
-  //     makeCellVisible(cell)
-  //     updateChunk(map.chunks, p[0] | 0, p[1] | 0)
-  //   }
-  // }
-
-  return map
 }
