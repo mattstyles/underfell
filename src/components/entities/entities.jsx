b2410bdd7eaa43235dbdf67b98937e8844104c05
Matt Styles <matt@personalurban.com>
2017-01-18 08:37:14 +0000

    fix translate entities to their correct screen position

--- src/components/entities/entities.jsx
+++ src/components/entities/entities.jsx
@@ -4,6 +4,7 @@ import {BLOCK_STATES, SIZES} from 'core/constants/game'
 
 const Entities = ({entities, map, translate}) => {
   let mat = ndarray(map.data, [map.width, map.height])
+  // @TODO translate entity position by translate, see previous commit
   let e = entities
     .filter(entity => {
       // To save calling mat.get twice we'll mutate here too, naughty
@@ -14,8 +15,8 @@ const Entities = ({entities, map, translate}) => {
     .map((entity, i) => {
       let style = {
         color: entity.color,
-        left: entity.position[0] * SIZES.CELL_WIDTH,
-        top: entity.position[1] * SIZES.CELL_HEIGHT,
+        left: (entity.position[0] + translate[0]) * SIZES.CELL_WIDTH,
+        top: (entity.position[1] + translate[1]) * SIZES.CELL_HEIGHT,
         opacity: entity.light
       }
       return (
